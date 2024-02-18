b77eb513074e3e395761313623b06930e133e140
Chirag Patel <chiragpatel41296@gmail.com>
2024-02-18 18:00:36 +0530

    Initial commit

---
+++ customer/src/services/customer-service.test.js
@@ -0,0 +1,10 @@
+//which service it is 
+describe("CustomerService", () => {
+    //which function 
+    describe("SignIn", () => {
+        //which scenario we are testing
+        test("validate user inputs", () => { });
+
+        test("Validate response", async () => { });
+    })
+}) 
---
+++ shopping/src/services/shopping-service.test.js
@@ -0,0 +1,10 @@
+//which service it is 
+describe("ShoppingService", () => {
+    //which function 
+    describe("PlaceOrder", () => {
+        //which scenario we are testing
+        test("validate user inputs", () => { });
+
+        test("Validate response", async () => { });
+    })
+}) 
